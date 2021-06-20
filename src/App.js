693a6d526ec57b1915900349d1ff3a3b4f956f09
abhishek raj <abhishekraj000012@gmail.com>
2021-06-21 00:27:19 +0530

    change

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import CartItem from './cartItem';
+import './App.css';
+import { reduce } from 'async';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        
+        <CartItem/>
+      </h1>
+    </div>
+  );
+}
+
+
+
+export default App;
