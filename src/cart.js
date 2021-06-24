2fc8bec48f815b34c0cebd0f060914cdd9670092
abhishek raj <abhishekraj000012@gmail.com>
2021-06-24 23:18:59 +0530

    several items

--- src/cart.js
+++ src/cart.js
@@ -3,12 +3,14 @@ import React from 'react';
 import CartItem from './cartItem';
 class Cart extends React.Component {
     render(){
+        //const arr=[1,2,3,4,5];
         return(
             <div className="cart-name"><CartItem/>
             <CartItem/>
             <CartItem/>
             <CartItem/>
-            <CartItem/></div>
+            <CartItem/>
+            </div>
         )
     }
 
