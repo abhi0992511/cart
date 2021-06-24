6157555a7ead19cc9d0c5e7b307410140a69ca1d
abhishek raj <abhishekraj000012@gmail.com>
2021-06-25 01:46:08 +0530

    add props

--- src/cart.js
+++ src/cart.js
@@ -2,14 +2,51 @@ import { directive } from '@babel/types';
 import React from 'react';
 import CartItem from './cartItem';
 class Cart extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+           products:[
+               {
+                price: 999,
+                tiltle: 'phone',
+                qty:1
+               },
+               {
+                price: 99,
+                tiltle: 'watch',
+                qty:10
+               },
+               {
+                price: 995,
+                tiltle: 'tab',
+                qty:12
+               },
+               {
+                price: 25252,
+                tiltle: 'tooth',
+                qty:13
+               }
+           ]
+        }
+        //this.increasequantity=this.increasequantity.bind(this);
+    }
     render(){
         //const arr=[1,2,3,4,5];
+        const {products}=this.state;
         return(
             <div className="cart-name"><CartItem/>
+           
+                {
+                    products.map((product)=>{
+                        return
+                            <CartItem product={product}/ >
+
+                        
+                    })
+                }
+            {/* <CartItem/>
             <CartItem/>
-            <CartItem/>
-            <CartItem/>
-            <CartItem/>
+            <CartItem/> */}
             </div>
         )
     }
--- src/cartItem.js
+++ src/cartItem.js
@@ -2,22 +2,14 @@ import { directive } from '@babel/types';
 import React from 'react';
 class CartItem extends React.Component {
 
-    constructor() {
-        super()
-        this.state = {
-            price: 999,
-            tiltle: 'phone',
-            qty:1
-        }
-        //this.increasequantity=this.increasequantity.bind(this);
-    }
+   
     increasequantity =() => {
         // this.state.qty+=1;
        // console.log('this.state',this.state);
        this.setState({
             qty:this.state.qty+1
         });
-        this.testing();
+       // this.testing();
     }
     decreasequantity=()=>{
         const {qty}=this.state;
@@ -31,37 +23,37 @@ class CartItem extends React.Component {
             }
         });
     }
-    testing()
-    {
-        const promise=new Promise((resolve,reject)=>{
-            setTimeout(()=>{
-                resolve('done');
-            },5000)
-        });
-        promise.then(()=>{
-            this.setState({
-                qty:this.state.qty+100
-            });
-            this.setState({
-                qty:this.state.qty+100
-            });
-            this.setState({
-                qty:this.state.qty+100
-            });
-        })
-    }
+    // testing()
+    // {
+    //     const promise=new Promise((resolve,reject)=>{
+    //         setTimeout(()=>{
+    //             resolve('done');
+    //         },5000)
+    //     });
+    //     promise.then(()=>{
+    //         this.setState({
+    //             qty:this.state.qty+100
+    //         });
+    //         this.setState({
+    //             qty:this.state.qty+100
+    //         });
+    //         this.setState({
+    //             qty:this.state.qty+100
+    //         });
+    //     })
+    // }
     
     render() {
-        //const { price, qty, title } = this.state;
+        const { price, qty, title } = this.state.product;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{ fontSize: 25, fontFamily: 'serif' }}>{this.state.price}</div>
-                    <div style={{ color: 'red', fontFamily: 'sans-serif' }}>{this.state.tiltle}</div>
-                    <div>qty:{this.state.qty}</div>
+                    <div style={{ fontSize: 25, fontFamily: 'serif' }}>{title}</div>
+                    <div style={{ color: 'red', fontFamily: 'sans-serif' }}>{price}</div>
+                    <div>qty:{qty}</div>
                     <div className="cart-item-actions">
                         <img alt="increase " className="action-icons" src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" onClick={this.increasequantity} />
                         <img alt="decrese " className="action-icons" src="https://t4.ftcdn.net/jpg/02/78/84/57/240_F_278845758_9xl3srVgd8p4jquxgxugGaHV1e5EOlLO.jpg" onClick={this.decreasequantity}/>
