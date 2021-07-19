b104e16680e7c38ee7ff9d3f61d35cf4f24c03f7
abhishek raj <abhishekraj000012@gmail.com>
2021-07-19 23:57:17 +0530

    complete

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+import 'firebase/firestore'
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB_Xh1RVZaJBekmnyaJwsUvILylqf5dHYo",
+  authDomain: "cart-c79d2.firebaseapp.com",
+  projectId: "cart-c79d2",
+  storageBucket: "cart-c79d2.appspot.com",
+  messagingSenderId: "363373684463",
+  appId: "1:363373684463:web:543a92308aa96f23ae1d1a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig); 
 ReactDOM.render(
   <React.StrictMode>
     <App />
